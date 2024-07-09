da5a20523a90070450852284dcd4549650e94fcb
Emil <emil.kelhala@gmail.com>
2024-07-09 16:21:45 +0300

    Remove template code, add heartbeat task

--- src/main.c
+++ src/main.c
@@ -4,32 +4,22 @@
 #include <pico/stdlib.h>
 #include <inttypes.h>
 
-void print_task(void * pvParams);
+void heartbeat_task(void * pvParams);
 
-//Delay for print task
-#define PRINT_DELAY 1000
+//Delay for heartbeat task
+#define HEARTBEAT_DELAY 1000
 
 int main() {
     stdio_init_all();
-    //Creating the print-task
-    xTaskCreate(
-        print_task,     //Task function to be run
-        "PRINT_TASK",   //Name of the task
-        1024,       //Stack depth to be allocated
-        NULL,       ///Arguments for the task - not needed right now
-        1,          //Task priority
-        NULL        //Task handle - not needed right now
-    );
-    //Start the scheduler and the tasks
+    xTaskCreate(heartbeat_task, "HEARTBEAT_TASK", 512, NULL, 1, NULL);
     vTaskStartScheduler();
     return 0;
 }
 
-void print_task(void *pvParams) {
+void heartbeat_task(void *pvParams) {
     while (true)
     {
-        printf("Hello world!\n");
-        //Wait for PRINT_DELAY amount of milliseconds
-        vTaskDelay(PRINT_DELAY / portTICK_PERIOD_MS);
+        printf("heartbeat_task\n");
+        vTaskDelay(HEARTBEAT_DELAY / portTICK_PERIOD_MS);
     }
 }
