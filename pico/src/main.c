a773d59cfb92612e6750f8bfc35cea5696db5402
Emil <emil.kelhala@gmail.com>
2024-07-13 23:53:10 +0300

    New model for sending data, connecting only when need to

--- pico/src/main.c
+++ pico/src/main.c
@@ -13,7 +13,6 @@
 #include "env.h"
 
 void publish_task(__unused void *pvParams);
-void network_task(__unused void *pvParams);
 void device_temp_task(__unused void *pvParams);
 
 void mqtt_connect_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status);
@@ -27,7 +26,6 @@ void mqtt_published_cb(void *arg, err_t error);
 
 #define PUBLISH_DELAY 2*MINUTE // How frequently we publish new data?
 
-#define NETWORK_CHECK_DELAY 30*1000
 
 static const struct mqtt_connect_client_info_t client_info = {
     "weather-station-1",
@@ -44,6 +42,7 @@ static mqtt_client_t* client;
 struct application_state {
     float device_temp;
     bool is_connected;
+    ip_addr_t server_ip;
 };
 
 struct application_state app_state;
@@ -51,7 +50,6 @@ struct application_state app_state;
 int main() {
     stdio_init_all();
     xTaskCreate(publish_task, "PUBLISH_TASK", 2048, NULL, 1, NULL);
-    xTaskCreate(network_task, "NETWORK_TASK", 2048, NULL, 1, NULL);
     xTaskCreate(device_temp_task, "DEVICE_TEMP_TASK", 512, NULL, 1, NULL);
     vTaskStartScheduler();
     return 0;
@@ -70,35 +68,52 @@ void mqtt_connect_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t 
 }
 
 void publish_task(__unused void *pvParams) {
-        while(true) {
-            if(app_state.is_connected) {
-                char temperature_value[4];
-                // Publish device temperature
-                sprintf(&temperature_value, "%.2f", app_state.device_temp);
-                mqtt_publish(client, TEMPERATURE_DEVICE_TOPIC, temperature_value, strlen(temperature_value), 0, 0, mqtt_published_cb, NULL);
+    // Initializations
+    if(cyw43_arch_init()) {
+        printf("Init failed!\n");
+    }
+    client = mqtt_client_new();
+    ip_addr_t ip;
+    ipaddr_aton(MQTT_SERVER_ADDR, &ip);
+    app_state.server_ip = ip;
+    while(true) {
+        err_t connect;
+        bool skip;
+        if(!app_state.is_connected) {
+            // Enable the wireless connectivity and connect to server
+            cyw43_arch_enable_sta_mode();
+            if(cyw43_arch_wifi_connect_blocking(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK)) {
+                printf("Failed to connect\n");
             }
-            vTaskDelay(PUBLISH_DELAY / portTICK_PERIOD_MS);
-        }
-}
-
-void network_task(__unused void *pvParameters) {
-        if(cyw43_arch_init()) {
-            printf("Init failed!\n");
-        }
-        cyw43_arch_enable_sta_mode();
-        if(cyw43_arch_wifi_connect_blocking(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK)) {
-            printf("Failed to connect\n");
-        }
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
-        client = mqtt_client_new();
-        ip_addr_t server_ip;
-        ipaddr_aton(MQTT_SERVER_ADDR, &server_ip);
-        while(true) {
-            if(!app_state.is_connected) {
-                mqtt_client_connect(client, &server_ip, MQTT_SERVER_PORT, mqtt_connect_cb, NULL, &client_info);
+            // Indicate that we are connected to wifi
+            cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+            // Now, contact server
+            ip = app_state.server_ip;
+            connect = mqtt_client_connect(client, &ip, MQTT_SERVER_PORT, mqtt_connect_cb, NULL, &client_info);
+            // We might get stuck here, make sure it does not happen
+            int tries = 0;
+            while(!app_state.is_connected) {
+                if(tries > 10) {
+                    // Set to timeout since we are getting nowhere
+                    connect = ERR_TIMEOUT;
+                    break;
+                }
+                tries++;
+                vTaskDelay(100/portTICK_PERIOD_MS);
             }
-            vTaskDelay(NETWORK_CHECK_DELAY / portTICK_PERIOD_MS);
         }
+        if(connect == ERR_OK) {
+            char temperature_value[4];
+            // Publish device temperature
+            sprintf(&temperature_value, "%.2f", app_state.device_temp);
+            mqtt_publish(client, TEMPERATURE_DEVICE_TOPIC, temperature_value, strlen(temperature_value), 0, 0, mqtt_published_cb, NULL);
+        }
+        mqtt_disconnect(client);
+        cyw43_arch_disable_sta_mode();
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+        app_state.is_connected = false;
+        vTaskDelay(PUBLISH_DELAY / portTICK_PERIOD_MS);
+    }
 }
 
 static float read_device_temp() {
