aa060bbdced77504870d6566551c4e8fbc0c01fe
Emil Kelhälä <emil.kelhala@gmail.com>
2024-07-20 00:39:08 +0300

    Add history search in any range

--- backend/api/history.js
+++ backend/api/history.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const Temperature = require('../models/Temperature');
+const Humidity = require('../models/Humidity');
 
 const router = express.Router();
 
-router.get('/temperature', async (req, res) => {
+const makeDBQuery = (start, end) => {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    if(startDate.getTime() > endDate.getTime()) {
+        throw new Error('Start date must be before end!')
+    }
+    return {timestamp: {$gt: new Date(start), $lt: new Date(end)}};
+}
 
+router.get('/temperature', async (req, res) => {
+    try {
+        res.json(await Temperature.find(makeDBQuery(req.query.start, req.query.end)))
+    }
+    catch(error) {
+        res.status(400).json({error: 'Bad request'})
+    }
 });
 
-router.get('/humidity', (req, res) => {
-
+router.get('/humidity', async (req, res) => {
+    try {
+        res.json(await Humidity.find(makeDBQuery(req.query.start, req.query.end)))
+    }
+    catch(error) {
+        res.status(400).json({error: 'Bad request'})
+    }
 });
 
 module.exports = router;
